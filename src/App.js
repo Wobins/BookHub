e9d3d3fb8209c6d1a7bfbb4930d666be0eea9918
Wobins <azalyange19@gmail.com>
2024-11-14 00:51:52 -0500

    Init repository

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Accueil from './pages/home';
+import Bibliotheque from './pages/books';
+import Prets from './pages/loans';
+import DefaultLayout from './layouts/default';
+import Borrowings from './pages/borrowings';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+  {
+    path: "/home",
+    element: <DefaultLayout children={<Accueil />} />,
+  },
+  {
+    path: "/books",
+    element: <DefaultLayout children={<Bibliotheque />} />,
+  },
+  {
+    path: "/loans",
+    element: <DefaultLayout children={<Prets />} />,
+  },
+  {
+    path: "/borrowings",
+    element: <DefaultLayout children={<Borrowings />} />,
+  },
+]);
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RouterProvider router={router} />
   );
 }
 
